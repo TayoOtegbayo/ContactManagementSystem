61a3a67501c5466f27c1cc8db32b93cf57f6c5f1
TayoOtegbayo <tayootegbayo@gmail.com>
2021-09-30 00:36:24 +0100

    Initial commit

--- GroceryMaintenanceSystem.java
+++ GroceryMaintenanceSystem.java
@@ -47,12 +47,12 @@ class Main{
 
     public static void printInstructions(){
         System.out.println("\nPress ");
-        System.out.println("\t 0 - To print choice options.");
-        System.out.println("\t 1 - To print items in the list of grocery items.");
-        System.out.println("\t 2 - To add an item to the list.");
-        System.out.println("\t 3 - To modify an item in the list.");
-        System.out.println("\t 4 - To remove an item from the list.");
-        System.out.println("\t 5 - To search an item in the list.");
+        System.out.println("\t 0 - To print choice options");
+        System.out.println("\t 1 - To print items in the list of grocery items");
+        System.out.println("\t 2 - To add an item to the list");
+        System.out.println("\t 3 - To modify an item in the list");
+        System.out.println("\t 4 - To remove an item from the list");
+        System.out.println("\t 5 - To search an item in the list");
         System.out.println("\t 6 - To copy array.");
         System.out.println("\t 7 - To quit the application.");
     }
